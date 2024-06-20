1be6f1efa1a402b74d434712b0c417438d786b39
Jareer <jforeverything2007@gmail.com>
2024-06-20 22:23:07 +0200

    Update

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const lexer = @import("./lexer/lexer.zig");
 const files = @import("./files/files.zig");
 
 pub fn main() anyerror!void {
-    const code = try files.read_file_clean("./coverage/main.re");
+    const code = try files.read_file_clean("./coverage/main.rv");
     std.debug.print("{s}\n\n[------------------ ^ RAW CODE ^ ------------------]\n\n", .{code});
 
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -21,5 +21,13 @@ pub fn main() anyerror!void {
         }
     } else |_| {
         std.debug.print("Lexer Failed...", .{});
+    }
+
+    // Stop it from dying early
+    while (true) {
+        const stdin = std.io.getStdIn().reader();
+        var buf: [10]u8 = undefined;
+
+        if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |_| {} else {}
     }
 }

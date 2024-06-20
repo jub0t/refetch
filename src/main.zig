402515590002442ca0e4539142b5c7f61c686d9c
Jareer <jforeverything2007@gmail.com>
2024-06-20 13:45:11 +0200

    Removes

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const lexer = @import("./lexer/lexer.zig");
 pub fn main() !void {
     const code: []const u8 = "print('Hi')";
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+
     defer arena.deinit();
     var allocator = arena.allocator();
 

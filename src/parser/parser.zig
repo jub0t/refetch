37438cc3538b926241ef62a561f3bba081508460
Jareer <jforeverything2007@gmail.com>
2024-06-22 13:13:21 +0200

    Update parser.zig

--- src/parser/parser.zig
+++ src/parser/parser.zig
@@ -1,3 +1,5 @@
+// The Parser.
+
 const Token = @import("../lexer/lexer.zig").Token;
 const std = @import("std");
 
@@ -12,22 +14,61 @@ pub const Operators = enum {
     EQADD, // +=
 };
 
-pub const Vunion = union {
-    Boolean: bool,
-    String: []u8,
-    Number: f64,
+pub const Vunion = union(enum) {
+    const Self = @This();
+
+    Boolean: ?bool,
+    String: ?[]u8,
+    Number: ?f64,
+    Null: ?bool,
+
+    pub fn to_string(me: Self) []const u8 {
+        return me.String;
+    }
 };
 
 pub const NodeAst = struct {
-    value: Vunion,
+    const Self = @This();
+
+    value: ?Vunion,
     type: InstructionType,
 
-    operator: Operators,
+    // Binary Operation.
+    operator: ?Operators,
+    lhs: ?*NodeAst,
+    rhs: ?*NodeAst,
 
     // Recursive stupidity
-    condition: *NodeAst,
-    then: *NodeAst,
-    otherwise: *NodeAst, // "else" is a zig keyword
+    condition: ?*NodeAst,
+    then: ?*NodeAst,
+    otherwise: ?*NodeAst, // "else" is a zig keyword
+
+    pub fn new(itype: InstructionType) Self {
+        const node = Self{
+            .type = itype,
+            .condition = null,
+            .value = null,
+            .lhs = null,
+            .rhs = null,
+            .otherwise = null,
+            .operator = null,
+            .then = null,
+        };
+
+        return node;
+    }
+
+    pub fn set_value(me: *NodeAst, value: Vunion) void {
+        me.value = value;
+    }
+
+    pub fn set_value_null(me: *NodeAst) void {
+        me.value = Vunion{ .Null = false };
+    }
+
+    pub fn get_value(me: *NodeAst) ?Vunion {
+        return me.value;
+    }
 };
 
 pub fn Parse(tokens: []Token) anyerror![]NodeAst {
@@ -35,6 +76,17 @@ pub fn Parse(tokens: []Token) anyerror![]NodeAst {
     var allocator = arena.allocator();
 
     const nodes = try allocator.alloc(NodeAst, 1024);
+    var n_idx: usize = 0; // Node Index;
+
+    var node = NodeAst.new(InstructionType.IdentAssign);
+    node.set_value_null();
+    node.set_value(Vunion{ .Null = false });
+
+    const val = node.get_value();
+    std.debug.print("Vunion {}\n", .{val.?});
+
+    nodes[n_idx] = node;
+    n_idx += 1;
 
     for (tokens) |token| {
         if (token.value == null) {
